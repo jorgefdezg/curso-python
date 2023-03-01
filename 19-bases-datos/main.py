06944fecd1c7cf84da73e8e6343615387d031b3e
Jorge Fernandez Gonzalez <jorge.fernandez3@udc.es>
2023-03-01 10:44:48 +0100

    Secciones sobre sqlite  y mysql basicos terminados

---
+++ 19-bases-datos/main.py
@@ -0,0 +1,68 @@
+import mysql.connector
+
+#Conexion
+database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="master_python"
+)
+
+#Cursor
+cursor = database.cursor(buffered=True)
+"""
+#Crear base de datos
+cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
+
+cursor.execute("SHOW DATABASES")
+"""
+#Crear tablas
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vehiculos(
+id int(10) auto_increment not null,
+marca varchar(40) not null,
+modelo varchar(40) not null,
+precio float(10,2) not null,
+CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+)
+""")
+
+# cursor.execute("SHOW TABLES")
+
+# for tables in cursor:
+#     print(tables)
+
+#cursor.execute("INSERT INTO vehiculos VALUES(null,'Opel','Astra',18500)")
+coches = [
+    ("Seat","Ibiza",8000),
+    ("Renault","Clio",15000),
+    ("Citroen","Saxo",2000),
+    ("Mercedes","Clase C",35000)
+]
+
+#cursor.executemany("INSERT INTO vehiculos VALUES(null,%s,%s,%s)",coches)
+
+database.commit()
+
+cursor.execute("SELECT * FROM vehiculos")
+result = cursor.fetchall()
+
+for coche in result:
+    print(coche[1],coche[3])
+
+
+cursor.execute("SELECT * FROM vehiculos")
+coche = cursor.fetchone()
+print(coche)
+
+#Borrar
+cursor.execute("DELETE FROM vehiculos WHERE marca = 'Mercedes'")
+database.commit()
+
+print(cursor.rowcount,"borrados!!")
+
+#Actualizar
+cursor.execute("UPDATE vehiculos SET modelo='Leon' WHERE marca= 'Seat'")
+database.commit()
+
+print(cursor.rowcount,"actualizados!!")
---
+++ 19-bases-datos/sqlite.py
@@ -0,0 +1,63 @@
+#Importar modulo
+import sqlite3
+
+#Conexion
+conexion = sqlite3.connect("pruebas.db")
+
+#Crear cursor
+cursor = conexion.cursor()
+
+#Crear tabla
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS productos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo varchar (255),             
+    descripcion text,
+    precio int(255)
+);
+""")
+
+#Guardar cambios
+conexion.commit()
+
+#Insertar datos
+"""
+cursor.execute("INSERT INTO productos VALUES (null, 'Segundo producto', 'Decripcion de mi producto', 550)")
+conexion.commit()
+"""
+
+#Borrar registros
+cursor.execute("DELETE FROM productos")
+conexion.commit()
+
+#Insertar muchos registros de golpe
+productos = [
+    ("Ordenador portatil", "Buen PC",600),
+    ("Telefono movil", "Buen telefono",140),
+    ("Placa base", "Buena placa",80),
+    ("Tablet 15", "Buena tablet",300),
+]
+cursor.executemany("INSERT INTO productos VALUES (null,?,?,?)", productos)
+conexion.commit()
+
+#Update
+cursor.execute("UPDATE productos SET precio=678 WHERE precio = 80;")
+
+#Lectura de datos
+cursor.execute("SELECT * FROM productos WHERE precio >=300;")
+productos = cursor.fetchall()
+
+for producto in productos:
+    print("ID:",producto[0])
+    print("Titulo:",producto[1])
+    print("Descripcion:",producto[2])
+    print("Precio:",producto[3])
+    print("--------------------")
+
+cursor.execute("SELECT titulo FROM productos")
+producto = cursor.fetchone()
+print(producto)
+
+
+#Cerrar conexion
+conexion.close()
