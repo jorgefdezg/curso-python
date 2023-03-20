4c81e2b4df2b246f65abf9440885a40699c25b67
Jorge Fernandez Gonzalez <jorge.fernandez3@udc.es>
2023-03-20 12:28:18 +0100

    Creada nueva app para entradas de blog

---
+++ 22-django/ProyectoDjango/blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    description = models.CharField(max_length=255,verbose_name="Descripción")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
+
+    class Meta():
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+    
+    def __str__(self):
+        return self.name
+    
+
+class Article(models.Model):
+    title = models.CharField(max_length=150,verbose_name="Título")
+    content = RichTextField(verbose_name = "Contenido")
+    image = models.ImageField(default="null", verbose_name="Miniatura")
+    public = models.BooleanField(verbose_name="¿Publicado?")
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="Categorías", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Editado el") 
+
+    class Meta():
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos"
+    
+    def __str__(self):
+        return self.title
