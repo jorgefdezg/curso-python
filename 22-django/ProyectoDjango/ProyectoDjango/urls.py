9c1f46552a1fb74a2b0b754c099e59d91e67d632
Jorge Fernandez Gonzalez <jorge.fernandez3@udc.es>
2023-03-21 17:19:00 +0100

    Listado de articulos en html creado, migraciones hechas y cambios en la manera de tratar al usuario que crea el articulo

--- 22-django/ProyectoDjango/ProyectoDjango/urls.py
+++ 22-django/ProyectoDjango/ProyectoDjango/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainapp.urls')),
-    path('',include('pages.urls'))
+    path('',include('pages.urls')),
+    path('',include('blog.urls'))
 ]
 
 
--- 22-django/ProyectoDjango/blog/views.py
+++ 22-django/ProyectoDjango/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from blog.models import Category, Article
+
 
 # Create your views here.
+def list(request):
+    
+    articles = Article.objects.all()
+
+    return render(request, 'articles/list.html',{
+        'title': 'Artículos',
+        'articles': articles
+    })
